a2ca59f7a7a50a380c9fe882daf631daf613757b
Bianka Adorján <bianka.adorjan@stud.uni-corvinus.hu>
2021-10-29 10:05:46 +0200

    dates

--- MNBSoap/MNBSoap/Form1.cs
+++ MNBSoap/MNBSoap/Form1.cs
@@ -7,14 +7,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MNBSoap.ServiceReference1;
 
 namespace MNBSoap
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
+            Consume();
+        }
+
+        void Consume()
+        {
+            MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient();
+            GetExchangeRatesRequestBody request = new GetExchangeRatesRequestBody();
+            request.currencyNames = "EUR";
+            request.startDate = "2020-01-01";
+            request.endDate = "2020-06-30";
         }
     }
 }
