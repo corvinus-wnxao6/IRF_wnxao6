b55915c1485780c2e00755301f069d8822ba9cd7
Bianka Adorján <bianka.adorjan@stud.uni-corvinus.hu>
2021-11-26 10:51:00 +0100

    PasswordTest

--- UnitTest/UnitTestProjectExample.Test/AccountControllerTestFixture.cs
+++ UnitTest/UnitTestProjectExample.Test/AccountControllerTestFixture.cs
@@ -28,5 +28,25 @@ namespace UnitTestExample.Test
             //Assert -- megfelel-e az eredmény amit vártam
             Assert.AreEqual(result, expectedResult);
         }
+
+        [
+            Test,
+            TestCase("abcd1234", false),
+            TestCase("ABCD123", false),
+            TestCase("abcDABCD",false),
+            TestCase("abD2",false),
+            TestCase("AbcdEF1234",true)
+        ]
+        public void TestValidatePassword(string password, bool expectedResult)
+        {
+            //Arrange
+            var accountController = new AccountController();
+
+            //Act
+            var result = accountController.ValidatePassword(password);
+
+            //Assert -- megfelel-e az eredmény amit vártam
+            Assert.AreEqual(result, expectedResult);
+        }
     }
 }
