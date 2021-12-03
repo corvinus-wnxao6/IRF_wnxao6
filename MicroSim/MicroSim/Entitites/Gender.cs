e401615bc34edf42f355012f409aa13278c2b540
Bianka Adorján <bianka.adorjan@stud.uni-corvinus.hu>
2021-12-03 10:18:04 +0100

    hello

---
+++ MicroSim/MicroSim/Entitites/Gender.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroSim.Entitites
+{
+    public enum  Gender
+    {
+        Male=1,
+        Female=2
+
+            //Gender ávaNeme= (Gender)2; kódban pedig a female lenne rögzítve pl. ifben ==female
+    }
+}
