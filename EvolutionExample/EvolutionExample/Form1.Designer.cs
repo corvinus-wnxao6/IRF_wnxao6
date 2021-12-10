9d00516588f6b0347c390702ac9eadcfffbf45c2
Bianka Adorján <bianka.adorjan@stud.uni-corvinus.hu>
2021-12-10 20:51:22 +0100

    igen

--- EvolutionExample/EvolutionExample/Form1.Designer.cs
+++ EvolutionExample/EvolutionExample/Form1.Designer.cs
@@ -29,13 +29,47 @@ namespace EvolutionExample
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblGenereation = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblGenereation
+            // 
+            this.lblGenereation.AutoSize = true;
+            this.lblGenereation.Location = new System.Drawing.Point(252, 62);
+            this.lblGenereation.Name = "lblGenereation";
+            this.lblGenereation.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblGenereation.Size = new System.Drawing.Size(46, 17);
+            this.lblGenereation.TabIndex = 0;
+            this.lblGenereation.Text = "label1";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(235, 101);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblGenereation);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblGenereation;
+        private System.Windows.Forms.Button btnStart;
     }
 }
 
--- EvolutionExample/EvolutionExample/Form1.cs
+++ EvolutionExample/EvolutionExample/Form1.cs
@@ -7,14 +7,86 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WorldsHardestGame;
 
 namespace EvolutionExample
 {
     public partial class Form1 : Form
     {
+        GameController gc = new GameController();
+        GameArea ga = null;
+        Brain winnerBrain = null;
+
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
+
         public Form1()
         {
             InitializeComponent();
+            gc.GameOver += Gc_GameOver;
+
+            ga = gc.ActivateDisplay();
+            this.Controls.Add(ga);
+
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
+
+            gc.Start();
+        }
+
+        private void Gc_GameOver(object sender)
+        {
+            generation++;
+            lblGenereation.Text = generation.ToString() + ".generáció";
+
+            var playerlist = from p in gc.GetCurrentPlayers()
+                             orderby p.GetFitness() descending
+                             select p;
+            var topPerformers = playerlist.Take(playerlist.Count() / 2).ToList();
+
+            var winners = from p in topPerformers
+                          where p.IsWinner
+                          select p;
+
+            if (winners.Count() > 0)
+            {
+                winnerBrain = winners.FirstOrDefault().Brain.Clone();
+                gc.GameOver -= Gc_GameOver;
+                btnStart.Visible = true;
+                return;
+            }
+
+            gc.ResetCurrentLevel();
+            foreach (var p in topPerformers)
+            {
+                var brain = p.Brain.Clone();
+                if (generation % 3 == 0)
+                    gc.AddPlayer(brain.ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(brain);
+
+                if (generation % 3 == 0)
+                    gc.AddPlayer(brain.Mutate().ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(brain.Mutate());
+            }
+            gc.Start();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            gc.ResetCurrentLevel();
+            gc.AddPlayer(winnerBrain.Clone());
+            gc.AddPlayer();
+
+            ga.Focus();
+
+            gc.Start(true);
+
         }
     }
 }
